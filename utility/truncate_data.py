6a23dba8e2dfbb792ecd1b8646325e3fe8e0d60c
riju18 <rijuchowdhury18@gmail.com>
2023-12-21 22:41:31 +0600

    [UPDATE] update table truncation problem

--- utility/truncate_data.py
+++ utility/truncate_data.py
@@ -1,7 +1,7 @@
 def truncate_data(insert_data):
     def wrapper(*args):
         _, _, tgt_conn, tgt_cur, _, tgt_schema, tgt_table = args  # src_conn, src_cur, tgt_conn, tgt_cur, tgt_table
-        truncate_sql = f"""truncate table {tgt_schema}.{tgt_table} ;"""
+        truncate_sql = f"""TRUNCATE TABLE {tgt_schema}.{tgt_table} RESTART IDENTITY CASCADE;"""
         tgt_cur.execute(truncate_sql)
         tgt_conn.commit()
         print(f"{tgt_schema}.{tgt_table} table data is truncated ...")
